3d8329a705f7243573778db1cbea684b6e6d93cb
Pálmai Mercédesz <palmaim@hotmail.com>
2019-07-29 15:29:34 +0200

    Hotfix heroku 3

--- src/main/java/com/progmatic/petsitterproject/services/UserService.java
+++ src/main/java/com/progmatic/petsitterproject/services/UserService.java
@@ -16,19 +16,18 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
-import javax.mail.MessagingException;
-import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  * @author progmatic
  */
 @Service
+@Transactional(readOnly = true)
 public class UserService {
 
     private UserRepo ur;
