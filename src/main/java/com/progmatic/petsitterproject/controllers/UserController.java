46eba23e100000319a12eb93f9b2eea899687f24
Imaginifer <povakri@hotmail.com>
2019-07-30 20:09:32 +0200

    Fixed search

--- src/main/java/com/progmatic/petsitterproject/controllers/UserController.java
+++ src/main/java/com/progmatic/petsitterproject/controllers/UserController.java
@@ -84,12 +84,12 @@ public class UserController {
 
     @GetMapping(value = "/sitters/search")
     public List<SitterViewDTO> listSitters(
-            @RequestParam(value = "name", defaultValue = "") String sitterName,
-            @RequestParam(value = "placeOfService", required = false) PlaceOfService placeOfService,
-            @RequestParam(value = "petType", required = false) PetType petType,
-            @RequestParam(value = "postCode", defaultValue = "0") int postCode
+            @RequestParam(value = "name", defaultValue="") String sitterName,
+            @RequestParam(value = "placeOfService", defaultValue="") String placeOfService,
+            @RequestParam(value = "petType", defaultValue="") String petType,
+            @RequestParam(value = "postCode", defaultValue="") String postCode
     ) {
-        SearchCriteriaDTO criteria = new SearchCriteriaDTO(sitterName, postCode, placeOfService, petType);
+        SearchCriteriaDTO criteria = new SearchCriteriaDTO(sitterName, decipherPostcode(postCode), decipherPlace(placeOfService), decipherPetType(petType));
         List<SitterViewDTO> selectedSitters = us.filterSitters(criteria);
         return selectedSitters;
     }
@@ -132,6 +132,27 @@ public class UserController {
                         .getProfilePhoto()
                         .getPic());
     }
+    
+    private PlaceOfService decipherPlace(String place){
+        if(place.isEmpty()){
+            return null;
+        }
+        return PlaceOfService.valueOf(place);
+    }
+    private PetType decipherPetType(String petType){
+        if(petType.isEmpty()){
+            return null;
+        }
+        return PetType.valueOf(petType);
+    }
+    
+    private int decipherPostcode(String code){
+        if(code.isEmpty()){
+            return 0;
+        }
+        return Integer.parseInt(code);
+    }
+    
 //    private SitterViewDTO convertToDTO(User user, Sitter sitter) {
 //        SitterViewDTO response = new SitterViewDTO();
 //        response.setProfilePhoto(sitter.getProfilePhoto());
